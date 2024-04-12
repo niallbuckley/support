fffe7a59ee9483f0ab3b7ba96fc1f3f960cb3b6e
Niall Buckley <buckleyniall100@gmail.com>
2024-04-12 11:13:46 -0500

    Remove top margin empty space

--- support-tool/src/App.vue
+++ support-tool/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <the-navigation></the-navigation>
-  <body>
+  <main>
   <img alt="Vue logo" src="./assets/logo.png">
 -  <HelloWorld msg="Welcome to Your Vue.js App"/>
-   </body>
+  </main>
 </template>
 
 <script>
@@ -27,6 +27,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
