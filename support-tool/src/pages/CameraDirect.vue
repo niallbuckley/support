67429570dac8d8a486a35dbb81c30b373b80e1f7
Niall Buckley <buckleyniall100@gmail.com>
2024-04-15 17:56:55 -0500

    Basic loading http testing

--- support-tool/src/pages/CameraDirect.vue
+++ support-tool/src/pages/CameraDirect.vue
@@ -1,8 +1,75 @@
 <template>
     <router-view></router-view>
     <h1> Camera Direct </h1>
-  </template>
-  
+    <div>
+      <button @click="loadExperiences">Load Submitted Experiences</button>
+    </div>
+    <p v-if="isLoading">Loading...</p>
+    <p v-else-if="!isLoading && error">{{ error }}</p>
+    <p v-else-if = "!isLoading">{{ error }}</p>
+    <!-- <p
+      v-else-if="!isLoading && (!results || results.length === 0)"
+    >No stored experiences found. Start adding some survey results first.</p> -->
+    <ul v-else>
+      <survey-result
+        v-for="result in results"
+        :key="result.id"
+        :name="result.name"
+        :rating="result.rating"
+      ></survey-result>
+    </ul>
+</template>
+
+<script>
+export default {
+  components: {
+    //SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+      isLoading: false,
+      error: null,
+    };
+  },
+  methods: {
+    loadExperiences() {
+      this.isLoading = true;
+      this.error = "OK this is awesome";
+      fetch('url', {headers:{"cookie":"auth_key=null"}})
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          this.isLoading = false;
+          const results = [];
+          for (const id in data) {
+            console.log("id: " + id);
+            results.push({id});
+            // results.push({
+            //   id: id,
+            //   name: data[id].name,
+            //   rating: data[id].rating,
+            // });
+          }
+          this.results = results;
+        })
+        .catch((error) => {
+          console.log(error);
+          this.isLoading = false;
+          this.error = 'Failed to fetch data - please try again later.';
+          console.log("HEREEE")
+        });
+    },
+  }
+  // mounted() {
+  //   this.loadExperiences();
+  // },
+};
+</script>
+
   <style scoped>
   li {
     margin: 1rem 0;
