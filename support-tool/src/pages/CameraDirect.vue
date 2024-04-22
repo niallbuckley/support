3f3e2934ab08e7c644da499f1cb86bb384192d7e
Niall Buckley <buckleyniall100@gmail.com>
2024-04-22 15:41:42 -0500

    Add camera instance call

--- support-tool/src/pages/CameraDirect.vue
+++ support-tool/src/pages/CameraDirect.vue
@@ -40,10 +40,30 @@ export default {
       searchInput: '',
       error: null,
       esnPattern : /^[0-9A-Fa-f]{8}$/,
-      macAddressPattern : /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/
+      macAddressPattern : /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/,
+      macAddress: ''
     };
   },
   methods: {
+    callCameraInstance(macAddress, hostName, port){
+      console.log("Call Cam Instance", macAddress, hostName, port);
+      axios.get(`http://localhost:9992/api/v2/CameraDirect/CameraInstance?address=${encodeURIComponent(macAddress)}&cluster_host=${hostName}&port=${port}`)
+        .then(response => {
+          if (response.status === 200) {
+            //this.errorGdi = null;
+            return response.data;
+          }
+        }).then(data => {
+          if (data.data.currentState.added===true || data.data.lastAddAttemptInfo.deviceId !== 0 ){
+            console.log("ds", data.data);
+            //display the data.
+          }
+        })
+        .catch(error => {
+          console.error(error);
+      });
+    },
+
     loadDevice(si) {
       var url = "";
       this.searchInput = si;
@@ -53,7 +73,8 @@ export default {
         url = "esn=" + this.searchInput;
       }
       else if (this.isMac(this.searchInput)) {
-        url = "mac_addr=" + encodeURIComponent(this.searchInput);
+        this.macAddress = this.searchInput;
+        url = "mac_addr=" + encodeURIComponent(this.macAddress);
       }
       else{
         // error handling
@@ -73,13 +94,14 @@ export default {
           const results = [];
           results.push(data.data);
           this.results = results;
+          this.macAddress = data.data.mac_address;
+          // TODO: if macAddress exists (it was passed in) and macAddress does not equal this mac Address display warning.
         })
         .catch(error => {
           console.error(error);
           this.isLoadingVms = false;
           this.error = `Could not get device data, ${error.response.status} status code ${error.response.data.Message || error.response.data.message}`;
         });
-
       axios.get(`http://localhost:9992/api/v2/CameraDirect/GlobalDispatchInfo?${url}`)
         .then(response => {
           if (response.status === 200) {
@@ -98,6 +120,20 @@ export default {
           this.errorGdi = `Could not get device data, ${error.response.status} status code ${error.response.data.Message || error.response.data.message}`;
         });
 
+        axios.get(`http://localhost:9992/api/v2/CameraDirect/EurekaInfo`)
+        .then(response => {
+          if (response.status === 200) {
+            //this.errorGdi = null;
+            return response.data;
+          }
+        }).then(data => {
+          for (const cluster of data.data){
+            this.callCameraInstance(this.macAddress, cluster.hostName, cluster.port);
+          }
+        })
+        .catch(error => {
+          console.error(error);
+        });
     },
 
     isEsn(identifier) {
