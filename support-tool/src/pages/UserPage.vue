e28300b5d96b945d14490a1ddc947f17a2172f08
Niall Buckley <buckleyniall100@gmail.com>
2024-05-02 14:51:14 -0500

    Make get request on User render or if param is changed

--- support-tool/src/pages/UserPage.vue
+++ support-tool/src/pages/UserPage.vue
@@ -8,21 +8,54 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
-      userId: ''
+      userId: '',
+      isLoadingVms: false,
+      vmsResults: []
+    }
+  },
+  methods: {
+    displayUserInfo(){
+      this.isLoadingVms = true;
+      console.log("hello", this.userId);
+      // Get user info from VMS
+      axios.get(`http://localhost:9992/api/v2/User/${this.userId}/Info`)
+        .then(response => {
+          if (response.status === 200) {
+            this.error = null;
+            return response.data;
+          }
+        }).then(data => {
+          this.isLoadingVms = false;
+          const results = [];
+          console.log(data.data);
+          results.push(data.data);
+          this.vmsResults = results;
+          console.log(this.vmsResults);
+        })
+        .catch(error => {
+          console.error(error);
+          this.isLoadingVms = false;
+          this.error = `Could not get device data, ${error.response.status} status code ${error.response.data.Message || error.response.data.message}`;
+        });
     }
   },
   watch: {
     '$route.params.id'(newId) {
       this.userId = newId;
+      console.log("here")
+      this.displayUserInfo();
     }
   },
   beforeMount() {
-    const userId = this.$route.params.id;
     this.userId = this.$route.params.id;
-    console.log(userId);
+  },
+  mounted(){
+    this.displayUserInfo();
   }
 };
 </script>
